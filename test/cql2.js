8e1b7b204242771c6a484d49413b0ff3068471f9
Kit Cambridge <github@kitcambridge.be>
2013-05-23 19:17:00 -0700

    Promisify the CQL 2 tests.

--- test/cql2.js
+++ test/cql2.js
@@ -1,269 +1,252 @@
-var poolConfig = require('./helpers/connection'),
-    badConfig = require('./helpers/bad_connection'), Helenus, conn,
-    config = require('./helpers/cql2'),
+var chai = require('chai'),
+    chaiAsPromised = require('chai-as-promised'),
+    P = require('p-promise');
+
+require('mocha-as-promised')();
+
+var scamandrios = require('../');
+
+var poolSettings = require('./helpers/connection.json'),
+    badSettings = require('./helpers/bad_connection.json'),
+    commands = require('./helpers/cql2.json'),
     canSelectCqlVersion = require('./helpers/can_select_cql_version');
 
-module.exports = {
-  'setUp':function(test, assert){
-    Helenus = require('helenus');
-    poolConfig.cqlVersion = '2.0.0';
+var assert = chai.assert,
+    expect = chai.expect;
 
-    function connect(){
-      conn = new Helenus.ConnectionPool(poolConfig);
-      conn.connect(function(err){
-        assert.ifError(err);
-        test.finish();
-      });
-    }
+chai.should();
+chai.use(chaiAsPromised);
 
-    canSelectCqlVersion(poolConfig, function(canSelect){
-      if (!canSelect){
-        console.error('set_cql_version not supported, unsetting cqlVersion');
-        delete poolConfig.cqlVersion;
-      }
-      connect();
-    });
-  },
+describe('Connection Pool', function ()
+{
+    var pool;
 
-  'test a bad connection will return an error':function(test, assert){
-     var badConn = new Helenus.ConnectionPool(badConfig);
-     // Add error handler to avoid uncaught exception.
-     badConn.on('error', function (err) { assert.isDefined(err); });
-     badConn.connect(function(err) {
-        assert.isDefined(err);
-        badConn.cql(config['create_ks#cql'], function(err, res){
-           assert.isDefined(err);
-           badConn.close();
-           test.finish();
-         });
-     });
-  },
-
-  'test cql create keyspace':function(test, assert){
-     conn.cql(config['create_ks#cql'], function(err, res){
-       assert.ifError(err);
-       assert.ok(res === undefined);
-       test.finish();
-     });
-  },
-
-  'test cql use keyspace':function(test, assert){
-     conn.cql(config['use#cql'], function(err, res){
-       assert.ifError(err);
-       assert.ok(res === undefined);
-       test.finish();
-     });
-  },
-
-  'test cql create column family':function(test, assert){
-     conn.cql(config['create_cf#cql'], function(err, res){
-       assert.ifError(err);
-       assert.ok(res === undefined);
-       test.finish();
-     });
-  },
-
-  'test cql create counter column family':function(test, assert){
-     conn.cql(config['create_counter_cf#cql'], function (err, res){
-       assert.ifError(err);
-       assert.ok(res === undefined);
-       test.finish();
-     });
-  },
-
-  'test cql create reversed column family':function(test, assert){
-     conn.cql(config['create_reversed_cf#cql'], function (err, res){
-       assert.ifError(err);
-       assert.ok(res === undefined);
-       test.finish();
-     });
-  },
-
-  'test cql update':function(test, assert){
-    conn.cql(config['update#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res === undefined);
-      test.finish();
-    });
-  },
-
-  'test cql update reversed':function(test, assert){
-    conn.cql(config['update_reversed#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res === undefined);
-      test.finish();
-    });
-  },
-
-  'test cql update with no callback':function(test, assert){
-    conn.cql(config['update#cql']);
-
-    //just wait to see if anything bad happens
-    setTimeout(function(){
-      test.finish();
-    }, 100);
-  },
-
-  'test cql incr':function(test, assert){
-    conn.cql(config['incr#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res === undefined);
-      test.finish();
-    });
-  },
-
-  'test cql select':function(test, assert){
-    conn.cql(config['select#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res.length === 1);
-      assert.ok(res[0] instanceof Helenus.Row);
-      assert.ok(res[0].get('foo').value === 'bar');
-      test.finish();
-    });
-  },
-
-  'test cql select reversed':function(test, assert){
-    conn.cql(config['select_reversed#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res.length === 1);
-      assert.ok(res[0] instanceof Helenus.Row);
-      assert.ok(res[0].get('foo').value === 'bar');
-
-      test.finish();
-    });
-  },
-
-  'test cql select *': function(test, assert){
-    conn.cql(config['select*#cql'], function (err, res){
-      assert.ifError(err);
-      assert.ok(res.length === 1);
-      assert.ok(res[0] instanceof Helenus.Row);
-      assert.ok(res[0].get('foo').value === 'bar');
-      test.finish();
-    });
-  },
-
-  'test cql select counter':function(test, assert){
-    conn.cql(config['select_counter#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res.length === 1);
-      assert.ok(res[0] instanceof Helenus.Row);
-      assert.ok(res[0].get('foo').value === 10);
-      test.finish();
-    });
-  },
-
-  'test cql incr and select':function(test, assert){
-    conn.cql(config['incr#cql'], function(err, res){
-      assert.ifError(err);
-      conn.cql(config['select_counter#cql'], function(err, res){
-        assert.ifError(err);
-        assert.ok(res.length === 1);
-        assert.ok(res[0] instanceof Helenus.Row);
-        assert.ok(res[0].get('foo').value === 20);
-        test.finish();
-      });
-    });
-  },
-
-  'test cql select with bad user input':function(test, assert){
-    var select = "SELECT foo FROM cql_test WHERE KEY='?'";
-
-    conn.cql(select, ["'foobar"], function(err, res){
-      assert.ifError(err);
-      assert.ok(res.length === 1);
-      assert.ok(res[0] instanceof Helenus.Row);
-      assert.ok(res[0].key === "'foobar");
-      assert.ok(res[0].count === 0);
-      test.finish();
-    });
-  },
-
-  'test cql count':function(test, assert){
-    conn.cql(config['count#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res.length === 1);
-      assert.ok(res[0] instanceof Helenus.Row);
-      assert.ok(res[0].get('count').value === 1);
-      test.finish();
-    });
-  },
-
-  'test cql error':function(test, assert){
-    conn.cql(config['error#cql'], function(err, res){
-      assert.ok(err instanceof Error);
-      assert.ok(res === undefined);
-      assert.ok(err.name === 'HelenusInvalidRequestException');
-      assert.ok(err.message.length > 0);
-      test.finish();
-    });
-  },
-
-  'test cql count with gzip':function(test, assert){
-    conn.cql(config['count#cql'], {gzip:true}, function(err, res){
-      assert.ifError(err);
-      assert.ok(res.length === 1);
-      assert.ok(res[0] instanceof Helenus.Row);
-      assert.ok(res[0].get('count').value === 1);
-      test.finish();
-    });
-  },
-
-  'test cql delete':function(test, assert){
-    conn.cql(config['delete#cql'], function(err, res){
-      assert.ifError(err);
-      assert.ok(res === undefined);
-      //after the delete check that all the columns have been deleted,
-      //we cant use a count here because the row will still remain until compaction occurs
-      //see http://www.datastax.com/docs/1.0/dml/about_writes#about-deletes
-      conn.cql(config['select2#cql'], config['select2#vals'], function(err, res){
-        assert.ifError(err);
-        assert.ok(res.length === 1);
-        assert.ok(res[0] instanceof Helenus.Row);
-        assert.ok(res[0].count === 0);
-        test.finish();
-      });
-    });
-  },
-
-  'test cql drop column family':function(test, assert){
-    conn.cql(config['drop_cf#cql'], function(err, res){
-       assert.ifError(err);
-       assert.ok(res === undefined);
-       test.finish();
-    });
-  },
-
-  'test cql drop keyspace':function(test, assert){
-    conn.cql(config['drop_ks#cql'], function(err, res){
-       assert.ifError(err);
-       assert.ok(res === undefined);
-       test.finish();
-    });
-  },
-
-  'test too many cql params':function(test, assert){
-    assert.throws(function(){
-      conn.cql(config['select2#cql'], [1,2,3,4,5,6]);
-    }, function(err){
-      return err.message === 'Too Many Parameters Given';
+    before(function ()
+    {
+        poolSettings.cqlVersion = '2.0.0';
+        return canSelectCqlVersion(poolSettings).then(function (canSelect)
+        {
+            if (!canSelect)
+            {
+                console.error('The `cqlVersion` cannot be set.');
+                delete poolSettings.cqlVersion;
+            }
+            pool = new scamandrios.ConnectionPool(poolSettings);
+            return pool.connect().should.be.fulfilled;
+        });
     });
 
-    test.finish();
-  },
+    describe('#connect()', function ()
+    {
+        it('should reject connections to nonexistent endpoints', function ()
+        {
+            var badPool = new scamandrios.ConnectionPool(badSettings);
+            badPool.on('error', function (error)
+            {
+                expect(error).to.exist;
+            });
 
-  'test too few cql params':function(test, assert){
-    assert.throws(function(){
-      conn.cql(config['select2#cql'], []);
-    }, function(err){
-      return err.message === 'Too Few Parameters Given';
+            return badPool.connect().should.be.rejected.then(function ()
+            {
+                return badPool.cql(commands['create_ks#cql']).should.be.rejected;
+            }).then(function ()
+            {
+                badPool.close();
+            });
+        });
+
+        it('can create a keyspace', function ()
+        {
+            return pool.cql(commands['create_ks#cql']).should.be.fulfilled;
+        });
+
+        it('can use a keyspace', function ()
+        {
+            return pool.cql(commands['use#cql']).should.be.fulfilled;
+        });
+
+        it('can create a column family', function ()
+        {
+            return pool.cql(commands['create_cf#cql']).should.be.fulfilled;
+        });
+
+        it('can create a column family holding counters', function ()
+        {
+            return pool.cql(commands['create_counter_cf#cql']).should.be.fulfilled;
+        });
+
+        it('can create a column family with a reversed comparator', function ()
+        {
+            return pool.cql(commands['create_reversed_cf#cql']).should.be.fulfilled;
+        });
+
+        it('can update a value in a column family', function ()
+        {
+            return pool.cql(commands['update#cql']).should.be.fulfilled;
+        });
+
+        it('can update a value in a reversed column family', function ()
+        {
+            return pool.cql(commands['update_reversed#cql']).should.be.fulfilled;
+        });
+
+        it('can increment a counter column value', function ()
+        {
+            return pool.cql(commands['incr#cql']).should.be.fulfilled;
+        });
+
+        it('can read a record from a column family', function ()
+        {
+            var promise = pool.cql(commands['select#cql']).should.be.fulfilled;
+            return promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.get('foo').value;
+            }).should.eventually.equal('bar');
+        });
+
+        it('can read a record from a reversed column family', function ()
+        {
+            var promise = pool.cql(commands['select_reversed#cql']).should.be.fulfilled;
+            return promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.get('foo').value;
+            }).should.eventually.equal('bar');
+        });
+
+        it('can request all records from a column family', function ()
+        {
+            var promise = pool.cql(commands['select*#cql']).should.be.fulfilled;
+            return promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.get('foo').value;
+            }).should.eventually.equal('bar');
+        });
+
+        it('can read a record from a counter column family', function ()
+        {
+            var promise = pool.cql(commands['select_counter#cql']).should.be.fulfilled;
+            return promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.get('foo').value;
+            }).should.eventually.equal(10);
+        });
+
+        it('can increment a column value and retrieve the updated value', function ()
+        {
+            var promise = pool.cql(commands['incr#cql']).should.be.fulfilled.then(function ()
+            {
+                return pool.cql(commands['select_counter#cql']).should.be.fulfilled;
+            });
+            return promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.get('foo').value;
+            }).should.eventually.equal(20);
+        });
+
+        it('can return results for malformed queries', function ()
+        {
+            var select = "SELECT foo FROM cql_test WHERE KEY='?'",
+                promise = pool.cql(select, ["'foobar"]).should.be.fulfilled;
+            var row = promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            });
+            return row.should.eventually.be.an.instanceof(scamandrios.Row).
+                       should.eventually.have.property('key', "'foobar").
+                       should.eventually.have.property('count', 0);
+        });
+
+        it('can return the number of rows matching a query', function ()
+        {
+            var promise = pool.cql(commands['count#cql']).should.be.fulfilled;
+            return promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.get('count').value;
+            }).should.eventually.equal(1);
+        });
+
+        it('can reject invalid CQL', function ()
+        {
+            return pool.cql(commands['error#cql']).should.be.rejected.with(Error);;
+        });
+
+        it('can return query results with `gzip` enabled', function ()
+        {
+            var promise = pool.cql(commands['count#cql'], { 'gzip': true }).should.be.fulfilled;
+            return promise.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.get('count').value;
+            }).should.eventually.equal(1);
+        });
+
+        it('can delete a column from a row', function ()
+        {
+            var rows = pool.cql(commands['delete#cql']).should.be.fulfilled.then(function ()
+            {
+                return pool.cql(commands['select2#cql'], commands['select2#vals']).should.be.fulfilled;
+            });
+            return rows.should.eventually.have.property('length', 1).then(function (value)
+            {
+                return value[0];
+            }).should.eventually.be.an.instanceof(scamandrios.Row).then(function (row)
+            {
+                return row.count;
+            }).should.eventually.equal(0);
+        });
+
+        it('can drop a column family', function ()
+        {
+            return pool.cql(commands['drop_cf#cql']).should.be.fulfilled;
+        });
+
+        it('can drop a keyspace', function ()
+        {
+            return pool.cql(commands['drop_ks#cql']).should.be.fulfilled;
+        });
+
+        it('throws if too many CQL parameters are given', function ()
+        {
+            expect(function ()
+            {
+                pool.cql(commands['select2#cql'], [1, 2, 3, 4, 5, 6]);
+            }).to.throw(/Too Many Parameters Given/);
+        });
+
+        it('throws if too few CQL parameters are given', function ()
+        {
+            expect(function ()
+            {
+                pool.cql(commands['select2#cql'], []);
+            }).to.throw(/Too Few Parameters Given/);
+        });
+
+        after(function ()
+        {
+            var deferred = P.defer();
+            pool.on('close', deferred.resolve);
+            pool.close();
+            return deferred.promise;
+        });
     });
-
-    test.finish();
-  },
-
-  'tearDown':function(test, assert){
-    conn.close();
-    test.finish();
-  }
-};
+});
