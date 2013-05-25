43f6d85facc5f9f73a24f27293d089593bc10917
C J Silverio <ceejceej@gmail.com>
2013-05-25 11:09:18 -0700

    Ported the units test (which is just a test for the node-uuid wrapper right now) to mocha.
    Turned on coverage requirements for travis; we require at least 75%
    coverage.
    
    Removed the old coverage report from the repo & added ours.

--- test/units.js
+++ test/units.js
@@ -1,24 +1,33 @@
-var Helenus;
+/*global describe:true, it:true, before:true, after:true */
 
-module.exports = {
-  'setUp':function(test, assert){
-    Helenus = require('helenus');
-    test.finish();
-  },
-  
-  'test uuid library': function(test, assert){
-    var v4 = new Helenus.UUID('e59578a0-bf3f-47b4-bcf0-94f9279271cc'),
-        v4buf = new Buffer([0xe5, 0x95, 0x78, 0xa0, 0xbf, 0x3f, 0x47, 0xb4, 0xbc, 0xf0, 0x94, 0xf9, 0x27, 0x92, 0x71, 0xcc]);
-        
-    assert.ok(v4.hex === 'e59578a0-bf3f-47b4-bcf0-94f9279271cc');
-    assert.ok(v4.toBinary() === v4buf.toString('binary'));
-    assert.ok(v4.toString() === 'e59578a0-bf3f-47b4-bcf0-94f9279271cc');
-    
-    var v1 = new Helenus.TimeUUID.fromTimestamp(new Date(1326400762701)),
-        v1buf = v1.toBuffer();
-    
+var
+		chai = require('chai'),
+		assert = chai.assert,
+		expect = chai.expect,
+		util = require('util'),
+		scamandrios = require('../')
+		;
 
-    assert.ok(v1.hex === (new Helenus.TimeUUID.fromBinary(v1buf.toString('binary')).hex));
-    test.finish();
-  },
-};
+chai.should();
+
+describe('units', function()
+{
+	it('UUID successfully wraps node-uuid v4', function()
+	{
+		var v4 = new scamandrios.UUID('e59578a0-bf3f-47b4-bcf0-94f9279271cc');
+		var v4buf = new Buffer([0xe5, 0x95, 0x78, 0xa0, 0xbf, 0x3f, 0x47, 0xb4, 0xbc, 0xf0, 0x94, 0xf9, 0x27, 0x92, 0x71, 0xcc]);
+
+		v4.hex.should.equal('e59578a0-bf3f-47b4-bcf0-94f9279271cc');
+		v4.toBinary().should.equal(v4buf.toString('binary'));
+		v4.toString().should.equal('e59578a0-bf3f-47b4-bcf0-94f9279271cc');
+	});
+
+	it('also wraps uuid-v1', function()
+	{
+		var v1 = new scamandrios.TimeUUID.fromTimestamp(new Date(1326400762701)),
+			v1buf = v1.toBuffer();
+
+		v1.hex.should.equal(new scamandrios.TimeUUID.fromBinary(v1buf.toString('binary')).hex);
+	});
+
+});
