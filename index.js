660ee296405a015f83102755ee990e5155a28524
C J Silverio <ceejceej@gmail.com>
2013-06-04 10:21:33 -0700

    Clean up how we require submodules from lib & export them in our module.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
-module.exports = require('./lib/helenus');
+module.exports =
+{
+    version          : require('./package').version,
+    ConnectionPool   : require('./lib/pool'),
+    Connection       : require('./lib/connection'),
+    Keyspace         : require('./lib/keyspace'),
+    ColumnFamily     : require('./lib/column_family'),
+    Column           : require('./lib/column'),
+    CounterColumn    : require('./lib/counter_column'),
+    Row              : require('./lib/row'),
+    Marshal          : require('./lib/marshal'),
+    UUID             : require('./lib/uuid').UUID,
+    TimeUUID         : require('./lib/uuid').TimeUUID,
+    ConsistencyLevel : require('./lib/cassandra/cassandra_types').ConsistencyLevel,
+};
