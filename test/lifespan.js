7b31e44a777f00ea29c1291556a82921527d2a06
C J Silverio <ceejceej@gmail.com>
2013-08-22 11:04:01 -0700

    Thrift connections managed by scamandrios now have a lifespan.
    At the end of the lifespan, they are torn down & replaced. This should
    be transparent to the user of a scamandrios client. The lifespan defaults
    to 20 minutes, but you may override this by setting a `TTL` field
    on the connection & connection pool options objects.
    
    Connection pools randomly alter the lifespan of each of their
    connections so they do not all time out at once. This jitter is set
    to 10 seconds right now. We might need to widen this.
    
    There is also a small set of tests for this feature.
    
    Bumped the version to 0.2.0.

---
+++ test/lifespan.js
@@ -0,0 +1,114 @@
+/*global describe:true, it:true, before:true, after:true */
+
+var
+    chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect,
+    should = chai.should(),
+    _ = require('lodash'),
+    P = require('p-promise'),
+    util = require('util')
+    ;
+
+var
+    scamandrios = require('../index'),
+    Connection = scamandrios.Connection,
+    poolConfig = require('./helpers/connection')
+    ;
+
+
+var testConn;
+
+describe('connection lifespan', function()
+{
+    before(function()
+    {
+        // mangle the pool object a bit
+        poolConfig.cqlVersion = '3.0.0';
+        poolConfig.host = poolConfig.hosts[0];
+    });
+
+    it('Connection.TTL should default to 20 minutes', function()
+    {
+        Connection.should.have.property('TTL');
+        assert.equal(Connection.TTL, 20 * 60 * 1000, 'connection TTL is not 20 minutes');
+    });
+
+    it('options passed to the constructor can override the TTL', function()
+    {
+        var myopts = _.extend({}, poolConfig);
+        myopts.TTL = 5;
+        var c = new Connection(myopts);
+
+        assert.equal(c.TTL, myopts.TTL);
+    });
+
+    it('should set a timer on a successful connection', function(done)
+    {
+        var c = new Connection(poolConfig);
+        c.connect()
+        .then(function()
+        {
+            assert.ok(c.connectionLifespan);
+            assert.ok(c.connectionLifespan._idleTimeout);
+        })
+        .fail(function(err)
+        {
+            console.log(err);
+            should.not.exist(err);
+        }).done(function()
+        {
+            c.close();
+            done();
+        });
+    });
+
+    it('should tear down & rebuild the connection at the end of the TTL', function(done)
+    {
+        var myopts = _.extend({}, poolConfig);
+        myopts.TTL = 500;
+        var c = new Connection(myopts);
+        var original;
+
+        c.on('log', function(message)
+        {
+            if (message === 'successfully reconnected after connection end-of-life')
+            {
+                assert.ok(original !== c._connection);
+                c.close();
+                done();
+            }
+        });
+
+        c.connect()
+        .then(function()
+        {
+            original = c._connection;
+        })
+        .fail(function(err)
+        {
+            console.log(err);
+            should.not.exist(err);
+        }).done();
+    });
+
+    it('pools should jitter the lifespan of their connections', function()
+    {
+        poolConfig.hosts.push(poolConfig.hosts[0]);
+        poolConfig.hosts.push(poolConfig.hosts[0]);
+        poolConfig.hosts.push(poolConfig.hosts[0]);
+        poolConfig.hosts.push(poolConfig.hosts[0]);
+        poolConfig.hosts.push(poolConfig.hosts[0]);
+
+        var pool = new scamandrios.ConnectionPool(poolConfig);
+        var ttls = _.map(pool.hosts, function(h)
+        {
+            return pool.createConnection(h).TTL;
+        });
+
+        assert.ok(_.all(ttls, _.isNumber));
+        assert.ok(_.all(ttls, function(n) { return n >= Connection.TTL; }));
+        assert.equal(ttls.length, _.uniq(ttls).length);
+    });
+
+});
