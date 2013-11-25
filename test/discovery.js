345b2b6e113e2444938359755836f95c071e103c
C J Silverio <ceejceej@gmail.com>
2013-11-25 14:04:57 -0800

    Update discovery test.

--- test/discovery.js
+++ test/discovery.js
@@ -9,8 +9,8 @@ var
     util        = require('util')
     ;
 
-var seedNode = '172.16.50.24:9160';
-var fullRing = [ '172.16.0.10', '172.16.34.68', '172.16.50.24' ];
+var seedNode = '172.16.29.211:9160';
+var fullRing = [ '172.16.0.10', '172.16.34.68', '172.16.29.211' ];
 
 describe('ring discovery', function()
 {
@@ -38,7 +38,7 @@ describe('ring discovery', function()
 
         it('can take object input', function(done)
         {
-            var node = { host: '172.16.50.24', port: '9160' };
+            var node = { host: '172.16.29.211', port: '9160' };
             scamandrios.discover(node)
             .then(function(list)
             {
