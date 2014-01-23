17ea893c79f2561f7e5631e140a50915ecd21742
C J Silverio <ceejceej@gmail.com>
2014-01-23 14:57:27 -0800

    Some connection ping unit tests.

--- test/monitor.js
+++ test/monitor.js
@@ -89,6 +89,42 @@ describe('connection monitor', function()
         pool.connect();
     });
 
+    it('individual clients start ping timers', function(done)
+    {
+        var pieces = poolConfig.hosts[0].split(':');
+        var conn = new scamandrios.Connection({ host: pieces[0], port: pieces[1] });
+
+        demand(conn._pingTimer).be.null();
+        conn.connect()
+        .then(function()
+        {
+            conn._pingTimer.must.be.truthy(done);
+            conn.close();
+            done();
+        }).done();
+    });
+
+    it('ping is called when the timer fires', function(done)
+    {
+        this.timeout(5000);
+        var pieces = poolConfig.hosts[0].split(':');
+        scamandrios.Connection.prototype.pingInterval = 500;
+
+        var conn = new scamandrios.Connection({ host: pieces[0], port: pieces[1] });
+        conn.on('ping', function(healthy)
+        {
+            healthy.must.be.true();
+            conn.close();
+            done();
+        });
+
+        conn.connect()
+        .then(function()
+        {
+            // no-op
+        }).done();
+    });
+
     function failWhaleQuery(buf)
     {
         return P.reject('fail whale');
@@ -129,7 +165,7 @@ describe('connection monitor', function()
             stubbed.ping()
             .then(function()
             {
-                console.log('successful ping?')
+                throw new Error('this ping was supposed to fail!');
             })
             .fail(function(err)
             {
